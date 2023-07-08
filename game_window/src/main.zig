c92568fa377857a2167cf1ef6a5e9fbe1126a5d0
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-07 22:37:26 -0400

    created a game window which links with the SDL2 library

---
+++ game_window/src/main.zig
@@ -0,0 +1,29 @@
+const c = @cImport({
+    @cInclude("SDL2/SDL.h");
+});
+
+const screen_width = 800;
+const screen_height = 450;
+
+pub fn main() void {
+    _ = c.SDL_Init(c.SDL_INIT_VIDEO);
+    defer c.SDL_Quit();
+
+    var window = c.SDL_CreateWindow("SDL2 Window", c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, 640, 400, 0);
+
+    var renderer = c.SDL_CreateRenderer(window, 0, c.SDL_RENDERER_PRESENTVSYNC);
+    defer c.SDL_DestroyRenderer(renderer);
+
+    mainloop: while(true) {
+        var sdl_event: c.SDL_Event = undefined;
+        while (c.SDL_PollEvent(&sdl_event) != 0) {
+            switch(sdl_event.type) {
+                c.SDL_QUIT => break :mainloop,
+                else => {},
+            }
+        }
+
+        _ = c.SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+        _ = c.SDL_RenderClear(renderer);
+    }
+}
