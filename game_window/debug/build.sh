3d4b3e262ded27df5b7afbf34f40c36975e651cc
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-07 22:57:01 -0400

    split the bin directory into debug and production directories

--- game_window/debug/build.sh
+++ game_window/debug/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Debug, ReleaseSafe, ReleaseSmall, ReleaseFast are the options for -O
-zig build-exe ../src/main.zig -lc -lSDL2 -O Debug -fno-emit-bin
+zig build-exe ../src/main.zig -lc -lSDL2 -O Debug
