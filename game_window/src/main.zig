c1daa277e6c55b0c6b4e457edc8b7f5df8aefad0
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-08 00:36:57 -0400

    render an image in the game window

--- game_window/src/main.zig
+++ game_window/src/main.zig
@@ -2,17 +2,33 @@ const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
 
+const level_width = 640;
+const level_height = 400;
 const screen_width = 800;
 const screen_height = 450;
+const image_width = 40;
+const image_height = 40;
 
 pub fn main() void {
     _ = c.SDL_Init(c.SDL_INIT_VIDEO);
     defer c.SDL_Quit();
 
-    var window = c.SDL_CreateWindow("SDL2 Window", c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, 640, 400, 0);
+    const window = c.SDL_CreateWindow("SDL2 Window", c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, level_width, level_height, 0);
 
-    var renderer = c.SDL_CreateRenderer(window, 0, c.SDL_RENDERER_PRESENTVSYNC);
-    defer c.SDL_DestroyRenderer(renderer);
+    const rend = c.SDL_CreateRenderer(window, 0, c.SDL_RENDERER_ACCELERATED);
+    defer c.SDL_DestroyRenderer(rend);
+
+    const image_surface = c.SDL_LoadBMP("test.bmp");
+    defer c.SDL_FreeSurface(image_surface);
+
+    const image_texture = c.SDL_CreateTextureFromSurface(rend, image_surface);
+    defer c.SDL_DestroyTexture(image_texture);
+
+    const srcrect = c.SDL_Rect{.x = 0, .y = 0, .w = image_width, .h = image_height};
+    const dstreact = c.SDL_Rect{.x = 20, .y = 20, .w = image_width, .h = image_height};
+
+    // [ Red, Green, Blue, Alpha ]
+    _ = c.SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
 
     mainloop: while(true) {
         var sdl_event: c.SDL_Event = undefined;
@@ -23,9 +39,9 @@ pub fn main() void {
             }
         }
 
-        // [ Red, Green, Blue, Alpha ]
-        _ = c.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        _ = c.SDL_RenderClear(renderer);
-        c.SDL_RenderPresent(renderer);
+        // srcrect, dstrect
+        _ = c.SDL_RenderClear(rend);
+        _ = c.SDL_RenderCopy(rend, image_texture, &srcrect, &dstreact);
+        c.SDL_RenderPresent(rend);
     }
 }
