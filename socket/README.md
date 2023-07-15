7905543a130e26fb5a76197401c112150e0dd5b4
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-15 13:52:36 -0400

    created the server in the socket code example

---
+++ socket/README.md
@@ -0,0 +1,1 @@
+[basic-tcp-chat.zig](https://gist.github.com/andrewrk/34c21bdc1600b0884a3ab9fa9aa485b8)
---
+++ socket/src/main.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const net = std.net;
+const fs = std.fs;
+const os = std.os;
+
+pub fn main() !void {
+    // Create server
+    var server = net.StreamServer.init(.{});
+    defer server.deinit();
+
+    // Listener
+    try server.listen(net.Address.parseIp("127.0.0.1", 8080) catch unreachable);
+    std.debug.print("Listening at {any}\n", .{server.listen_address});
+
+    while (true) {
+        // Accept
+        _ = try server.accept();
+    }
+}
