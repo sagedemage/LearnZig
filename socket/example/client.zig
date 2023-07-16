ba0de3f39fdd8390f34bf3ba6784aaf293731eab
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-15 23:00:32 -0400

    removed the code for reading a buffer and documented the client code

--- socket/example/client.zig
+++ socket/example/client.zig
@@ -3,14 +3,12 @@ const std = @import("std");
 
 pub fn main() !void {
     const client_msg = "Hello";
-
-    // Create server
     const server_address = std.net.Address.initIp4([4]u8{ 127, 0, 0, 1 }, 8080);
+
+    // Connect to the Server
     const conn = try std.net.tcpConnectToAddress(server_address);
     defer conn.close();
 
+    // Send a message to the server
     _ = try conn.write(client_msg);
-
-    var buf: [1024]u8 = undefined;
-    _ = try conn.read(buf[0..]);
 }
