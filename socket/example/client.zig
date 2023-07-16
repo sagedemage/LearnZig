482ede00b5883f4cc2ab685d9dc6624be56d6477
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-15 22:44:19 -0400

    removed unused imports

--- socket/example/client.zig
+++ socket/example/client.zig
@@ -1,15 +1,12 @@
 /// Client
 const std = @import("std");
-const net = std.net;
-const fs = std.fs;
-const os = std.os;
 
 pub fn main() !void {
     const client_msg = "Hello";
 
     // Create server
-    const server_address = net.Address.initIp4([4]u8{ 127, 0, 0, 1 }, 8080);
-    const conn = try net.tcpConnectToAddress(server_address);
+    const server_address = std.net.Address.initIp4([4]u8{ 127, 0, 0, 1 }, 8080);
+    const conn = try std.net.tcpConnectToAddress(server_address);
     defer conn.close();
 
     _ = try conn.write(client_msg);
